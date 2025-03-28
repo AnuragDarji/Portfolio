2d617fc8dbab4d87a4e873c3f297eb9237be2312
AnuragDarji <127482974+AnuragDarji@users.noreply.github.com>
2025-03-28 11:12:54 +0530

    make custom cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+// ============ CURSOR ============
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function(e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  // cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  },{duration: 500, fill: "forwards"});
+})
+
 /*=============== SHOW SIDEBAR ===============*/
 const navMenu = document.getElementById("sidebar"),
   navToggle = document.getElementById("nav-toggle"),
